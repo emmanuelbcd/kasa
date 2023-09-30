3951497bd62957d2c7c2f3d39ee41c9c47bce29c
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-09-30 23:06:03 +0200

    Header

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from '../../components/Header/Header.jsx';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+//on exporte le composant Home pour qu'il puisse être utilisé
+export default Home;
