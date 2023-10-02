4639dcb5948f9433de187a3feb4b149f4d79f363
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-02 10:43:54 +0200

    update comments

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 //index est le point d'entrée de l'application react.
 //c'est le premier fichier qui s'exécute
 
+//on importe les modules nécessaires pour démarrer l'appli
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//on crée un root react
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//on "render" (ou on affiche) le composant App à l'intérieur de l'élément racine (root)
 root.render(
   <React.StrictMode>
       <App/>
