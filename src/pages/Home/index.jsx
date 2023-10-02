939097c0490a67a85b8e344f8b0b811df5afaa23
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-02 10:46:56 +0200

    update home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-
 import Header from '../../components/Header/Header.jsx';
+import Gallery from '../../components/Gallery/Gallery.jsx';
+import Banner from '../../components/Banner/Banner.jsx';
+import homeBanner from '../../assets/home-banner.png';
+import Footer from '../../components/Footer/Footer.jsx';
 
 function Home() {
     return (
-        <div>
+        <div className="home">
             <Header />
+            <Banner image={homeBanner} slogan="Chez vous, partout et ailleurs." />
+            <Gallery />
+            <Footer />
         </div>
     );
 }
