34490c7bd14c53f35c852c6f7e574b0fe4a02080
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-21 09:27:36 +0200

    link & uselocation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 //components/Navbar/Navbar.jsx
 
 import React from 'react'; //on importe la bibliothèque React pour pouvoir créer des composants react
-import { Link } from 'react-router-dom'; //on importe le composant Link depuis react-router-dom
+import { Link, useLocation } from 'react-router-dom'; //on importe le composant Link depuis react-router-dom
 
 //on définit le composant Navbar
 function Navbar() {
+    const location = useLocation(); //on souhaite obtenir l'emplacement
     return (
         <nav className="navbar">
             <ul>
-                <li><Link to="/">Accueil</Link></li>
-                <li><Link to="/a-propos">A propos</Link></li>
+                <li><Link to="/" className={location.pathname === "/" ? "active-link" : "" }>Accueil</Link></li>
+                <li><Link to="/a-propos" className={location.pathname === "/a-propos" ? "active-link" : ""}>A propos</Link></li>
             </ul>
         </nav>
     );
