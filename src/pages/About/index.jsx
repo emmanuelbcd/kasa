f2f01ce988f9583c425675434f5f0b0cac938d1e
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-22 21:23:57 +0200

    add about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Header from '../../components/Header/Header.jsx';
+import aboutData from '../../data/aboutData.json';
 import Banner from '../../components/Banner/Banner.jsx';
 import aboutBanner from '../../assets/about-banner.png';
+import Collapse from '../../components/Collapse/Collapse.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
+
 
 function About() {
     return (
         <div className="about">
             <Header />
             <Banner image={aboutBanner} />
+            {aboutData.map(data => (
+                <Collapse title={data.title} content={data.content} key={data.title} />
+            ))}
             <Footer />
         </div>
     );
