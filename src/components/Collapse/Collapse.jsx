aa403c963a02bbcdaf8cc0f4517895e1fd1139ec
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-22 21:24:55 +0200

    add collapse component

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,24 @@
+//components/Collapse/Collapse
+
+import { useState } from 'react';
+import Arrow from '../../assets/arrow.png'; //on importe l'image du logo
+
+function Collapse({ title, content }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="collapse">
+            <button className="collapse-title" onClick={() => setIsOpen(!isOpen)}>
+                {title}
+                <img 
+                    src={Arrow}
+                    alt="toggle" 
+                    className={`arrow-icon ${isOpen ? 'opened' : ''}`}
+                />
+            </button>
+            {isOpen && <div className="collapse-content opened">{content}</div>}
+        </div>
+    );
+}
+
+export default Collapse; 
