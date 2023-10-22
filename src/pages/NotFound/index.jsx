9d576964c3ba531e2b355925ec80a285130b4158
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-22 22:34:00 +0200

    notFound index update

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,17 @@
 //import React from 'react';
+import Header from '../../components/Header/Header.jsx';
 
 function NotFound() {
-    
+    return (
+        <div className="not-found-page">
+            <Header />
+            <div className="error-section">
+                <h1 className="error-code">404</h1>
+                <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+                <a href="/" className="return-home">Retourner sur la page d'accueil</a>
+            </div>
+        </div>
+    );
 }
 
 export default NotFound;
