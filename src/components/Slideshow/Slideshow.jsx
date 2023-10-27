e7a7117bd5f85f681687971157f041a587d7fedf
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-27 06:12:58 +0200

    Slideshow component

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -2,6 +2,8 @@
 
 //useState est un Hook de react qui permet d'ajouter un état local à un composant fonctionnel
 import React, { useState } from 'react'; 
+import Slideleft from '../../assets/slideshow-left.png'; //on importe la flèche gauche 
+import Slideright from '../../assets/slideshow-right.png'; //on importe la flèche droite
 
 //Slideshow est un composant fonctionnel de React
 //Il accepte une prop pictures qui est un tableau d'URLs d'images à afficher dans le diaporama
@@ -19,8 +21,22 @@ function Slideshow({ pictures }) {
     return (
         <div className="slideshow-container">
             <img src={pictures[currentIndex]} alt={`Slide ${currentIndex + 1}`} className="slide-image" />
-            <button onClick={goPrev} className="slide-button prev">←</button>
-            <button onClick={goNext} className="slide-button next">→</button>
+            {/* Condition pour afficher le compteur si le nombre d'images est supérieur à 1 */}
+            {pictures.length > 1 && (
+                <span className="slide-counter">{currentIndex + 1}/{pictures.length}</span>
+            )}
+            {/*Condition pour afficher la flèche précédente si le nombre d'images est supérieur à 1*/}
+            {pictures.length > 1 && (
+                <button onClick={goPrev} className="slide-button prev">
+                    <img src={Slideleft} alt="Précédent"/>
+                </button>
+            )}
+            {/*Condition pour afficher la flèche suivante si le nombre d'images est supérieur à 1*/}
+            {pictures.length > 1 && (
+            <button onClick={goNext} className="slide-button next">
+                <img src={Slideright} alt="Suivant"/>
+            </button>
+            )}
         </div>
     );
 }
