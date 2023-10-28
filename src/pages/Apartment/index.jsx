a541cd183f9afb9bce249d85ee46a9089a543ba7
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-10-28 21:31:38 +0200

    not found page

--- src/pages/Apartment/index.jsx
+++ src/pages/Apartment/index.jsx
@@ -3,24 +3,22 @@ import Header from '../../components/Header/Header.jsx';
 import Slideshow from '../../components/Slideshow/Slideshow.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
 
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 
 //import des données
 import apartmentsData from '../../data/apartments.json';
 
 function Apartment({ match }) {
     const {id} = useParams();
-    //const apartmentId = parseInt(id, 10);
     const apartment = apartmentsData.find(apt => apt.id === id);
 
     // Ajout des logs de débogage ici
     console.log("ID de l'URL:", id);
-    //console.log("ID recherché:", apartmentId);
     console.log("Appartement trouvé:", apartment);
     console.log("Données complètes des appartements:", apartmentsData);
 
     if (!apartment) {
-        return <div>Appartement non trouvé</div>; // ou toute autre logique pour gérer cette erreur
+        return <Navigate to="/not-found" replace />; // ou toute autre logique pour gérer cette erreur
     }
 
     return (
