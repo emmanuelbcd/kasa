5de1a4f2c3347894f033bf05c039c2fac1bb0926
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-11-12 21:40:46 +0100

    Navbar comments

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 //components/Navbar/Navbar.jsx
+//Le composant Navbar fournit une navigation entre différentes pages.
 
-import React from 'react'; //on importe la bibliothèque React pour pouvoir créer des composants react
-import { Link, useLocation } from 'react-router-dom'; //on importe le composant Link depuis react-router-dom
+//imports
+//on importe la bibliothèque React pour pouvoir créer des composants react
+import React from 'react'; 
+//on importe le composant Link et useLocation depuis react-router-dom.
+//Link est utilisé pour créer des liens navigables sans recharger la page
+//useLocation est un Hook qui permet d'accéder à l'objet location qui représente l'URL actuelle
+import { Link, useLocation } from 'react-router-dom'; 
 
 //on définit le composant Navbar
 function Navbar() {
@@ -9,7 +15,10 @@ function Navbar() {
     return (
         <nav className="navbar">
             <ul>
+                {/*on utilise un opérateur ternaire ?. */}
+                {/*condition: on vérifie si on est sur la page d'accueil */}
                 <li><Link to="/" className={location.pathname === "/" ? "active-link" : "" }>Accueil</Link></li>
+                {/*condition: on vérifie si on est sur la page à propos */}
                 <li><Link to="/a-propos" className={location.pathname === "/a-propos" ? "active-link" : ""}>A Propos</Link></li>
             </ul>
         </nav>
