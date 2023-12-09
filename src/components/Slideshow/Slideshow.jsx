31b4b8ede8c21b3e34b746bc98660326fc89cdf3
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-12-09 08:59:02 +0100

    comments

--- src/components/Slideshow/Slideshow.jsx
+++ src/components/Slideshow/Slideshow.jsx
@@ -7,14 +7,27 @@ import Slideright from '../../assets/slideshow-right.png'; //on importe la flèc
 
 //Slideshow est un composant fonctionnel de React
 //Il accepte une prop pictures qui est un tableau d'URLs d'images à afficher dans le diaporama
+
+//Slideshow reçoit "pictures" en tant que PROP
+//La PROP pictures est utilisée pour déterminer 
+//quelles images afficher dans le slideshow
+//et combien d'images il y a au total
 function Slideshow({ pictures }) {
+    // L'index du 1er slideshow est défini dans l'état intial useState
+    //currentIndex est la variable qui stocke l'index actuel de l'image affichée
     const[currentIndex, setCurrentIndex]=useState(0);
 
     const goNext = () => {
+        // cette ligne calcule le nouvel index pour l'image suivante 
+        // le modulo est utilisé pour créer un effet de boucle
+        // quand on atteint la fin du tableau, l'index revient à O
         setCurrentIndex(( prevIndex ) => ( prevIndex + 1 ) % pictures.length);
     }
 
     const goPrev = () => {
+        // cette ligne permet de revenir à l'image précédente
+        // le modulo est aussi utilisé pour permettre une navigation en boucle
+        // si l'index devient négatif, il boucle vers la fin du tableau
         setCurrentIndex(( prevIndex ) => ( prevIndex - 1 + pictures.length ) % pictures.length);
     }
 
