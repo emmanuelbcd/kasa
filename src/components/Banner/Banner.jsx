92b4da28204806ede5deec32527cdc51fd6aacfe
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-12-09 08:57:19 +0100

    comments

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,14 @@
 //compnents/Banner/Banner.jsx
 //import React from 'react';
 
+// le composant Banner reçoit 2 props : image et slogan
 function  Banner( { image, slogan } ) {
     return (
+        // un élément section est créé avec une class qui change en fonction de la présence ou non d'un slogan
+        // si un slogan est fourni, la class with slogan est utilisée
         <section className={`banner ${slogan ? "with-slogan" : "without-slogan"}`}>
             <img src={ image } alt="bannière" />
+            {/* on utilise une logique conditionnelle pour voir si slogan est présent. Si oui, création de h2 */}
             { slogan && <h2>{slogan}</h2>}
         </section>
     );
