f7b2c213f1e49d21f7af49ca63a1855b5193f86f
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-12-09 08:58:29 +0100

    comments

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import apartmentsData from '../../data/apartments.json';
+import apartmentsData from '../../data/apartments.json'; //importation du tableau contenant les info des appartements
 import Card from '../Card/Card.jsx';
 
 function Gallery() {
     return(
         <main className="gallery">
+            {/* la méthode map itère sur chaque objet dans apartmentsData */}
+            {/* pour chaque appartement, la méthode map crée un composant card */}
+            {/* chaque card reçoit des props : key, id, cover, title */}
             {apartmentsData.map(apartment => (
                 <Card   key={ apartment.id } 
                         id={apartment.id} 
