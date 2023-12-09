8de7a212c4096c5cd2a255752b61eec3750ae09a
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-12-09 08:59:31 +0100

    comments

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,3 +1,4 @@
+//importations
 import React from 'react';
 import Header from '../../components/Header/Header.jsx';
 import aboutData from '../../data/aboutData.json';
@@ -6,12 +7,15 @@ import aboutBanner from '../../assets/about-banner.png';
 import Collapse from '../../components/Collapse/Collapse.jsx';
 import Footer from '../../components/Footer/Footer.jsx';
 
-
+//structure de la fonction About
 function About() {
     return (
         <div className="about">
             <Header />
             <Banner image={aboutBanner} />
+            {/*aboutData.map : utilise la méthode map pour itérer sur chaque élément de aboutData*/}
+            {/*pour chaque élément contenant un title et un content, il crée un composant collapse*/}
+            {/*chaque collapse reçoit title, content et une key unique (le titre de l'élément */}
             {aboutData.map(data => (
                 <Collapse title={data.title} content={data.content} key={data.title} customClass="about_collapse" />
             ))}
